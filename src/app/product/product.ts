d645293c9685c689cad912ca1e06810cd2172cb5
jordan8080 <117922957+jordan8080@users.noreply.github.com>
2024-11-15 17:23:21 +0100

    bien

--- src/app/product/product.ts
+++ src/app/product/product.ts
@@ -3,11 +3,15 @@ export class Product {
   name: string;
   price: number;
   description: string;
+  image: string;
+  category: number;
 
-  constructor(id: number, name: string, price: number, description: string) {
+  constructor(id: number, name: string, price: number, description: string, image: string, category: number) {
     this.id = id;
     this.name = name;
     this.price = price;
     this.description = description;
+    this.image = image;
+    this.category = category;
   }
 }
